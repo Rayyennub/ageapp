c822b447f0f0d34a0945839f4f625338b83b17d2
jemblonganvalley <jemblonganvalley@gmail.com>
2021-02-15 19:48:41 +0700

    update

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+
+//buat sebuah method/function
+const handleSubmit = (event)=>{
+    //kita buat form static
+    event.preventDefault()
+
+    //tangkap nilai input dari user
+    let username = document.getElementById('username').value
+    let email = document.getElementById('email').value
+    let phone = document.getElementById('phone').value
+    let address = document.getElementById('address').value
+    let birthday = document.getElementById('birthday').value
+
+    console.log(`
+username : ${username}
+email : ${email}
+phone : ${phone}
+address : ${address}
+birthday : ${birthday}
+age : ${2021 - parseInt(birthday.split("-")[0]) + " Years Old"}
+    `)
+}
+
