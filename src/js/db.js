a1cad86afe3498c54001824b6daaf5f6152e4ad9
jemblonganvalley <jemblonganvalley@gmail.com>
2021-02-16 10:58:27 +0700

    web sql

---
+++ src/js/db.js
@@ -0,0 +1,3 @@
+var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+
+export default db
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,43 @@
+import db from './db.js'
+
+window.register = (data)=>{
+
+    const {username, email, phone, address, birthday, age} = data
+
+    db.transaction(function (tx) {
+        //   tx.executeSql('CREATE TABLE IF NOT EXIST foo (id unique, text)');
+        tx.executeSql('CREATE TABLE users (username unique, email unique, phone, address, birthday, age)');
+        //   tx.executeSql('DROP TABLE users');
+
+    });
+
+    db.transaction(function async (tx) {
+        tx.executeSql(`INSERT INTO users (username , email, phone, address, birthday, age) VALUES("${username}", "${email}", "${phone}", "${address}", "${birthday}", "${age}")`,[], (tx, result)=>{
+            if(result.rowsAffected === 1){
+                console.log('registrasi berhasil')
+                console.log(result)
+                return result.rowsAffected
+            }else{
+                console.log('error');
+                return result.rowsAffected
+            }
+            
+        });
+    });
+}
+
+window.getAllUsers = ()=>{
+    
+    db.transaction((tx)=>{
+        tx.executeSql(`SELECT * FROM users`,[],(tx, result)=>{
+            console.log(result.rows)
+        })
+    })
+}
+
 
 //buat sebuah method/function
-const handleSubmit = (event)=>{
+window.handleSubmit = (event)=>{
     //kita buat form static
     event.preventDefault()
 
@@ -10,14 +47,38 @@ const handleSubmit = (event)=>{
     let phone = document.getElementById('phone').value
     let address = document.getElementById('address').value
     let birthday = document.getElementById('birthday').value
+    
+    let data = {
+    username : username,
+    email : email,
+    phone : phone,
+    address : address,
+    birthday : birthday,
+    age : 2021 - parseInt(birthday.split("-")[0]) + " Years Old"
+        }
 
-    console.log(`
-username : ${username}
-email : ${email}
-phone : ${phone}
-address : ${address}
-birthday : ${birthday}
-age : ${2021 - parseInt(birthday.split("-")[0]) + " Years Old"}
-    `)
+    //check if localStorage availablel
+        
+    register(data)
+  
+    // window.location.reload()
+
 }
 
+const storeToLocal = async (data)=>{
+    const isStoredData = localStorage.getItem('data')
+    if(isStoredData){
+        let prevData = localStorage.getItem('data')
+        let ps = JSON.parse(prevData)
+        ps.push(data)
+        localStorage.setItem('data', JSON.stringify(ps))
+        // console.log(localStorage.getItem('data'))
+        // window.location.reload()
+
+    }else{
+        localStorage.setItem('data',JSON.stringify([data]))
+        console.log(localStorage.getItem('data'))
+        // window.location.reload()
+
+    }
+}
